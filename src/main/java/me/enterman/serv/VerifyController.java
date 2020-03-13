55c739a93cdf5a6e5829f4c64ccbf89a39d337c5
Linus Torvalds <torvalds@linux-foundation.org>
2020-03-13 08:00:06 +0000

    add

---
+++ src/main/java/me/enterman/serv/VerifyController.java
@@ -0,0 +1,18 @@
+package me.enterman.serv;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VerifyController {
+	@RequestMapping("/verify.php")
+	public String verify(@RequestParam(name = "helloWorld",required = false,defaultValue = "null") String hashed){
+		if(hashed.equals("null")){
+			return null;
+		}
+		// 这里写验证
+		return "";
+
+	}
+}
