730c172edc533a0b28cc9c4d57628b27ef9b6cc4
Linus Torvalds <torvalds@linux-foundation.org>
2020-03-13 08:09:39 +0000

    Update VerifyController.java

--- src/main/java/me/enterman/serv/controller/VerifyController.java
+++ src/main/java/me/enterman/serv/controller/VerifyController.java
@@ -1,4 +1,4 @@
-package me.enterman.serv;
+package me.enterman.serv.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
